84decf742b9bfa4fa038347c00663470e19026d4
sujit13666 <sujit13666@gmail.com>
2016-05-10 15:44:20 +0600

    User Management Done

---
+++ app/scripts/controllers/addUserCtrl.js
@@ -0,0 +1,50 @@
+(function () {
+    'use strict';
+
+    app
+        .controller('ChangePasswordCtrl', ChangePasswordCtrl);
+
+    ChangePasswordCtrl.$inject = ['identityService', 'userService', 'responseService', '$scope', '$state'];
+
+    function ChangePasswordCtrl(identityService, userService, responseService, $scope, $state) {
+
+
+        if(!$scope.$parent.loggedIn){
+            $state.go("app.login");
+        }
+
+        $scope.$parent.headerStyle = "dark";
+        $scope.$parent.activePage = "user";
+
+
+        $scope.changePassword = _changePassword;
+
+
+        function _changePassword(valid) {
+            if(valid){
+                userService.changePassword($scope.user).then(function (response) {
+                    responseService.showSuccessToast(response.data.success.successTitle, response.data.success.successDescription);
+                    $state.go('app.dashboard');
+                }).catch(function(response){
+
+                    if (response.data.error_description == "The access token provided is invalid.") {
+
+                    } else if (response.data.error_description == "The access token provided has expired.") {
+                        identityService.getRefreshAccessToken(identityService.getRefreshToken()).then(function (response) {
+                            identityService.setAccessToken(response.data);
+                            _changePassword(valid);
+                        });
+                    } else if (response.data.error != undefined) {
+                        responseService.showErrorToast(response.data.error.errorTitle, response.data.error.errorDescription);
+                    } else {
+                        responseService.showErrorToast("Something Went Wrong", "Please Refresh the page again.")
+                    }
+
+                });
+
+            }
+
+        }
+    }
+
+})();
---
+++ app/scripts/services/adminUserService.js
@@ -0,0 +1,67 @@
+(function () {
+
+    'use strict';
+
+    app
+        .factory('userService', userService);
+
+    userService.$inject=['apiService','SERVER_CONSTANT','LOGIN_CONSTANT','USER_CONSTANT'];
+
+    function userService(apiService,SERVER_CONSTANT,LOGIN_CONSTANT,USER_CONSTANT) {
+
+        return {
+            getAuthorizedUserShortData: _getAuthorizedUserShortData,
+            getAuthorizedUserFullData: _getAuthorizedUserFullData,
+            setUpdatedSocialUserData:_setUpdatedSocialUserData,
+            getAllUserData: _getAllUserData,
+            saveUpdatedUserDataAdmin: _saveUpdatedUserDataAdmin,
+//            updateFullName:_updateFullName,
+//            updateUserUniversityCampus:_updateUserUniversityCampus,
+            updateUserProfile: _updateUserProfile,
+            changePassword: _changePassword,
+            getAllNonApprovedUsers:_getAllNonApprovedUsers
+        };
+
+        function _getAuthorizedUserShortData(accessToken){
+            return apiService.get(SERVER_CONSTANT.HOST+USER_CONSTANT.CURRENT_USER_SHORT_DETAILS+"?access_token="+accessToken);
+        }
+
+        function _getAuthorizedUserFullData(accessToken){
+            return apiService.get(SERVER_CONSTANT.HOST+USER_CONSTANT.CURRENT_USER_FULL_DETAILS+"?access_token="+accessToken);
+        }
+
+        function _setUpdatedSocialUserData(accessToken,data){
+            return apiService.post(SERVER_CONSTANT.HOST+USER_CONSTANT.UPDATE_SOCIAL_USER+"?access_token="+accessToken,data);
+        }
+
+        function _getAllUserData(accessToken){
+
+            return apiService.post(SERVER_CONSTANT.HOST+USER_CONSTANT.ADMIN_ALL_USER_DATA+"?access_token="+accessToken);
+        }
+
+        function _saveUpdatedUserDataAdmin(accessToken,data){
+            return apiService.post(SERVER_CONSTANT.HOST+USER_CONSTANT.ADMIN_UPDATE_USER_DATA+"?access_token="+accessToken,data);
+        }
+
+//        function _updateFullName(accessToken,data){
+//            return apiService.post(SERVER_CONSTANT.HOST+USER_CONSTANT.UPDATE_FULL_NAME+"?access_token="+accessToken,data);
+//        }
+//
+//        function _updateUserUniversityCampus(accessToken,data){
+//            return apiService.post(SERVER_CONSTANT.HOST+USER_CONSTANT.UPDATE_USER_UNIVERSITY_CAMPUS+"?access_token="+accessToken,data);
+//        }
+        function _updateUserProfile(accessToken,data){
+            return apiService.post(SERVER_CONSTANT.HOST+USER_CONSTANT.UPDATE_PROFILE+"?access_token="+accessToken,data);
+        }
+
+        function _changePassword(data){
+            return apiService.post(SERVER_CONSTANT.HOST+USER_CONSTANT.CHANGE_PASSWORD,data);
+        }
+        function _getAllNonApprovedUsers(accessToken,data){
+            return apiService.post(SERVER_CONSTANT.HOST+USER_CONSTANT.GET_ALL_NON_APPROVED_USER+"?access_token="+accessToken,data);
+        }
+
+
+    }
+
+})();
