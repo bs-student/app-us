9da4d4b983841fc606f535fc3ab07cff1ce62af6
sujit13666 <sujit13666@gmail.com>
2016-05-10 18:54:26 +0600

    Admin All Book Deals Done

---
+++ app/scripts/services/adminBookDealService.js
@@ -0,0 +1,44 @@
+(function () {
+
+    'use strict';
+
+    app
+        .factory('bookDealService', bookDealService);
+
+    bookDealService.$inject=['SERVER_CONSTANT','BOOK_DEAL_CONSTANT','apiService'];
+
+    function bookDealService(SERVER_CONSTANT,BOOK_DEAL_CONSTANT,apiService) {
+
+        return {
+            getBookDealsIhaveContactedFor:_getBookDealsIhaveContactedFor,
+            getBookDealsOfMine:_getBookDealsOfMine,
+            sellBookToUser: _sellBookToUser,
+            getBookDealsOfMineWhichAreSold:_getBookDealsOfMineWhichAreSold,
+            getBookDealsOfMineWhichAreBought: _getBookDealsOfMineWhichAreBought
+
+        };
+
+
+        function _getBookDealsIhaveContactedFor(accessToken){
+            return apiService.get(SERVER_CONSTANT.HOST+BOOK_DEAL_CONSTANT.GET_BOOK_DEALS_I_HAVE_CONTACTED_FOR+"?access_token="+accessToken);
+        }
+
+        function _getBookDealsOfMine(accessToken){
+            return apiService.get(SERVER_CONSTANT.HOST+BOOK_DEAL_CONSTANT.GET_BOOK_DEALS_I_HAVE_CREATED+"?access_token="+accessToken);
+        }
+
+        function _sellBookToUser(accessToken,data){
+            return apiService.post(SERVER_CONSTANT.HOST+BOOK_DEAL_CONSTANT.SELL_BOOK_TO_USER+"?access_token="+accessToken,data);
+        }
+
+        function _getBookDealsOfMineWhichAreSold(accessToken){
+            return apiService.get(SERVER_CONSTANT.HOST+BOOK_DEAL_CONSTANT.GET_BOOK_DEALS_I_HAVE_CREATED_AND_SOLD+"?access_token="+accessToken);
+        }
+
+        function _getBookDealsOfMineWhichAreBought(accessToken){
+            return apiService.get(SERVER_CONSTANT.HOST+BOOK_DEAL_CONSTANT.GET_BOOK_DEALS_I_HAVE_BOUGHT+"?access_token="+accessToken);
+        }
+
+    }
+
+})();
