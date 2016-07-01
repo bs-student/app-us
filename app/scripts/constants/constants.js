751b0aa185312ef8b819075a24050d18821533ee
sujit13666 <sujit13666@gmail.com>
2016-07-01 14:30:15 +0600

    Issue V1 rest Resolved

--- app/scripts/constants/constants.js
+++ app/scripts/constants/constants.js
@@ -4,10 +4,10 @@
 
     app
         .constant("SERVER_CONSTANT",{
-//            "HOST":"http://localhost:8080/Student2StudentApi/web/app_dev.php",
-//            "IMAGE_HOST_PATH":"./../../Student2StudentApi/web"
-              "HOST":"http://168.61.173.224:8080/Student2Student/api/web",
-            "IMAGE_HOST_PATH":"./api/web"
+            "HOST":"http://localhost:8080/Student2StudentApi/web/app_dev.php",
+            "IMAGE_HOST_PATH":"./../../Student2StudentApi/web"
+//              "HOST":"http://168.61.173.224:8080/Student2Student/api/web",
+//            "IMAGE_HOST_PATH":"./api/web"
         })
         .constant("API_CLIENT_CONSTANT", {
             "CLIENT_ID": "1_1t8rtqj36wkgcw4k8wko4wo808ksok0w8wssow4k8sgw08osw8",
