c30c76bcf69abf3d4506f07e63f683ab1d4428fc
sujit13666 <sujit13666@gmail.com>
2017-01-05 18:51:37 +0600

    Video in news management

--- gulpfile.js
+++ gulpfile.js
@@ -290,7 +290,7 @@ gulp.task("css_website", function (done) {
     return gulp.src(assets.css_website)
         .pipe(plugins.print())
         .pipe(plugins.cssmin())
-        .pipe(plugins.concat('app-2.8.min.css'))
+        .pipe(plugins.concat('app-2.9.min.css'))
         .pipe(gulp.dest('app/dist/css'));
 });
 
