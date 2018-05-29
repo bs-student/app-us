5e53561b40de2f5ae7f52010a612cf51768762be
sujit13666 <sujit.bit.0329@gmail.com>
2018-05-29 21:09:35 +0600

    Fixed- Cheque payment method is removed.

--- gulpfile.js
+++ gulpfile.js
@@ -310,7 +310,7 @@ gulp.task("css_website", function (done) {
     return gulp.src(assets.css_website)
         .pipe(plugins.print())
         .pipe(plugins.cssmin())
-        .pipe(plugins.concat('app-2.18.min.css'))
+        .pipe(plugins.concat('app-2.19.min.css'))
         .pipe(gulp.dest('app/dist/css'));
 });
 
